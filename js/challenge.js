a7b61adff8a23d962110a0ac0379deb7ea520daa
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-05 15:52:57 -0400

    got like stuff working

--- js/challenge.js
+++ js/challenge.js
@@ -1,26 +1,58 @@
 
 document.addEventListener("DOMContentLoaded", function(e) {
-
-let value = 0
-
+    const plusButton = document.getElementById("plus");
+    const minusButton = document.getElementById("minus");
+    let value = 0
     function setTimer () {
         let counterValue = document.getElementById("counter")
         value += 1
         counterValue.innerHTML = value
         setTimeout(setTimer, 1000);
-
     }
-
     setTimeout(setTimer, 1000);
 
+    let array = []
 
-const plusButton = document.getElementById("plus");
-const minusButton = document.getElementById("minus");
-
-
+    let likes = 1
 
 
+    document.addEventListener("click", function(e) {
 
-})
 
 
+        let counterValue = document.getElementById("counter")
+        array.push(parseInt(counterValue.textContent))
+
+        if (e.target.matches("#plus")) {
+            let countValue = parseInt(counterValue.textContent)
+            countValue += 1
+
+        } else if (e.target.matches("#minus")) {
+            let countValue = parseInt(counterValue.textContent)
+            countValue -= 1
+
+
+        } else if (e.target.matches("#heart")) {
+            let countValue = parseInt(counterValue.textContent)
+            let ul = document.querySelector(".likes")
+
+            let list = document.createElement('li')
+
+
+            parseInt(countValue)
+
+            console.log(array[-1])
+
+            if (array[array.length - 1] === array[array.length - 2] ) {
+                likes += 1
+            } else {
+                likes = 1
+            }
+
+            list.innerHTML = `${counterValue.textContent} has ${likes} likes.`
+            ul.append(list)
+
+
+        }
+    })
+    })
