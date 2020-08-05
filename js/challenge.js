e0b186ee71d406ea78eaf37e2a717cc049e2f9fb
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-05 14:20:44 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -4,31 +4,18 @@ document.addEventListener("DOMContentLoaded", function(e) {
 let value = 0
 
     function setTimer () {
-
         let counterValue = document.getElementById("counter")
         value += 1
-
         counterValue.innerHTML = value
-
-        console.log(value)
-
-
         setTimeout(setTimer, 1000);
-
 
     }
 
     setTimeout(setTimer, 1000);
 
 
-    const counterBox = document.getElementById("counter");
-    //setInterval(increaseTime, 100)
-
-
-    let counterButton = document.getElementById("counter")
-
-
-
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
 
 
 
