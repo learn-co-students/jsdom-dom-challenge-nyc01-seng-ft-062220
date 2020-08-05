4b803237d0e0f331288fa943c030fa1b4346b13d
roman9891 <roman9891@gmail.com>
2020-08-05 16:04:11 -0400

    all deliverables

---
+++ js/challenge.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function(e){
+    //global variables
+    const plusButton = document.getElementById('plus')
+    const minusButton = document.getElementById('minus')
+    const pauseButton = document.getElementById('pause')
+    const heartButton = document.getElementById('heart')
+    const submitButton = document.getElementById('submit')
+    const counter = document.getElementById('counter')
+    const likesSpace = document.querySelector(`ul.likes`)
+    let currentInterval = setInterval(incrementCounterText, 1000);
+    const incrementCounterText = () => {
+        counter.innerText = parseInt(counter.innerText) + 1
+    }
+    
+    //button events
+    minusButton.addEventListener('click', function(){
+        counter.innerText = parseInt(counter.innerText) - 1
+    })
+
+    plusButton.addEventListener('click', function(){
+        counter.innerText = parseInt(counter.innerText) + 1
+    })
+
+    heartButton.addEventListener('click', function(){
+        let likeCounter = 1
+        if (document.getElementById(counter.innerText)) {
+            
+            let currentListItem = document.getElementById(counter.innerText)
+            currentListItem.counter++
+            currentListItem.innerText = `${counter.innerText} has been liked ${currentListItem.counter} time(s)`
+            console.log('this is running', currentListItem.counter)
+        } else {
+            const newListItem = document.createElement('li')
+            newListItem.counter = 1
+            newListItem.innerText = `${counter.innerText} has been liked ${newListItem.counter} time(s)`
+            newListItem.id = counter.innerText
+            
+            likesSpace.append(newListItem)
+        }
+    })
+
+    submitButton.addEventListener('click', function(e){
+        e.preventDefault()
+        let inputText = document.getElementById('comment-input').value
+        let commentsDiv = document.getElementById('list')
+        let newComment = document.createElement('li')
+        newComment.innerText = inputText
+        commentsDiv.append(newComment)
+        document.getElementById('comment-input').value = ''
+    })
+    
+    pauseButton.addEventListener('click', function(){
+        if (pauseButton.innerText === 'pause') {
+            clearInterval(currentInterval)
+            pauseButton.innerText = 'resume'
+            heartButton.disabled = true;
+            plusButton.disabled = true;
+            minusButton.disabled = true;
+            submitButton.disabled = true;
+        } else {
+            currentInterval = setInterval(incrementCounterText, 1000)
+            pauseButton.innerText = 'pause'
+            heartButton.disabled = false;
+            plusButton.disabled = false;
+            minusButton.disabled = false;
+            submitButton.disabled = false;
+        }
+    })
+}); 
